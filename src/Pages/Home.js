2f43c4e6e8854fdecc159e26f26f25beefc42fb5
leocassiosilva <leocassiosilva1234@gmail.com>
2021-07-19 23:25:40 -0300

    colocando a logo do IF

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export function Home( { navigation} ){
   return(
       <View style={styles.container}>
-          <Text >
-              IFRN - Pau dos Ferros
+        <View style={styles.imgLogo}>
+          <Image
+              source={require('../img/logo.png')}
+              style={styles.logo}
+          />  
+          <Text style={styles.textLogo}>
+              Pau dos Ferros
           </Text>
+        </View>
+
+        <View>
+          <Text>
+              Button
+          </Text>
+        </View>
       </View>
   )
 }
@@ -15,8 +27,22 @@ export function Home( { navigation} ){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: '#FFF',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
+  imgLogo:{
+    height:200,
+    width:'100%',
+    alignItems:'center',
+  },
+  logo:{
+    height:132,
+    width:88,
+    alignItems:'center'
+  }, 
+  textLogo:{
+    paddingTop:20,
+    fontSize:30
+  }
 });
